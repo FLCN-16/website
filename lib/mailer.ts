2c381a0150981e9303bbacca5f7d67a07b14bad1
THE FALCON <57741232+FLCN-16@users.noreply.github.com>
2022-03-20 12:29:54 +0530

    Force env variables as string

--- lib/mailer.ts
+++ lib/mailer.ts
@@ -1,17 +1,17 @@
 import nodemailer from 'nodemailer';
 
 const mailer = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
+  host: process.env.MAIL_HOST as string,
+  port: process.env.MAIL_PORT as string,
   auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
+    user: process.env.MAIL_USER as string,
+    pass: process.env.MAIL_PASS as string,
   },
 });
 
 export const sendMail = (to: string, subject: string, html: string) => {
   return mailer.sendMail({
-    from: process.env.MAIL_FROM,
+    from: process.env.MAIL_FROM as string,
     to,
     subject,
     html,
