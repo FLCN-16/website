31b57da6772838cc3eaaf1c86386d5146fc4ca61
THE FALCON <57741232+FLCN-16@users.noreply.github.com>
2023-01-17 18:11:52 +0530

    Added GTM

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,8 +37,20 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800;900&display=swap"
           />
+          <Script
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{ __html: `!function(e,t,a,r,g){e[r]=e[r]||[],e[r].push({"gtm.start":new Date().getTime(),event:"gtm.js"});var n=t.getElementsByTagName(a)[0],s=t.createElement(a);s.async=!0,s.src="https://www.googletagmanager.com/gtm.js?id="+g+("dataLayer"!=r?"&l="+r:""),n.parentNode.insertBefore(s,n)}(window,document,"script","dataLayer","GTM-TLRRVXK");`}}
+          />
         </Head>
         <body>
+          {/* <!-- Google Tag Manager (noscript) --> */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-TLRRVXK"
+              height="0" width="0" style="display:none;visibility:hidden"
+            ></iframe>
+          </noscript>
+          {/* <!-- End Google Tag Manager (noscript) --> */}
           <Main />
           <NextScript />
         </body>
