276d673976b52544ecd62d15799200c5c20bdd27
The Falcon <57741232+FLCN-16@users.noreply.github.com>
2023-03-29 13:25:18 +0530

    Fixeed linting issue

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,6 +39,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800;900&display=swap"
           />
           <Script
+            id="gtm-script"
             strategy="afterInteractive"
             dangerouslySetInnerHTML={{ __html: `!function(e,t,a,r,g){e[r]=e[r]||[],e[r].push({"gtm.start":new Date().getTime(),event:"gtm.js"});var n=t.getElementsByTagName(a)[0],s=t.createElement(a);s.async=!0,s.src="https://www.googletagmanager.com/gtm.js?id="+g+("dataLayer"!=r?"&l="+r:""),n.parentNode.insertBefore(s,n)}(window,document,"script","dataLayer","GTM-TLRRVXK");`}}
           />
